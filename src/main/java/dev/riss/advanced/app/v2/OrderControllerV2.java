764f71e5d3a6da9f038018f3aad137f2041464a1
riss-study <dlrudghks651@naver.com>
2023-08-04 17:45:27 +0900

    [log_tracker] log trace V2 - sync trace id and apply level of callee method(depth) in same request

---
+++ src/main/java/dev/riss/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package dev.riss.advanced.app.v2;
+
+import dev.riss.advanced.trace.TraceStatus;
+import dev.riss.advanced.trace.helloTrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request (String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
